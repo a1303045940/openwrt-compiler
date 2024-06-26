21bbedce41bf60c75f46deb574d06a1c986e7c1b
Grinch27 <Grinch27@outlook.com>
2024-06-26 11:41:45 +0800

    Update diy-part2.sh

--- openwrt/diy-part2.sh
+++ openwrt/diy-part2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Old Luci
-echo "
-CONFIG_PACKAGE_luci-compat=y
-" >> .config
+# # Old Luci
+# echo "
+# CONFIG_PACKAGE_luci-compat=y
+# " >> .config
 
-# Add luci-app-adguardhome
-rm -rf package/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
-echo "
-CONFIG_PACKAGE_luci-app-adguardhome=y
-" >> .config
+# # Add luci-app-adguardhome
+# rm -rf package/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
+# echo "
+# CONFIG_PACKAGE_luci-app-adguardhome=y
+# " >> .config
 
 # https-dns-proxy
 echo "
