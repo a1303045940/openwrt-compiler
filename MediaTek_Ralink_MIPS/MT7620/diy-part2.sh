d90a257ce1a41b857c1f4d8b806902e1e6a4e156
Grinch27 <Grinch27@outlook.com>
2024-06-30 16:22:06 +0800

    Update diy-part2.sh

--- MediaTek_Ralink_MIPS/MT7620/diy-part2.sh
+++ MediaTek_Ralink_MIPS/MT7620/diy-part2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-# https-dns-proxy
-echo "
-CONFIG_PACKAGE_luci-app-https-dns-proxy=y
-CONFIG_PACKAGE_https-dns-proxy=y
-" >> .config
+# # https-dns-proxy
+# echo "
+# CONFIG_PACKAGE_luci-app-https-dns-proxy=y
+# CONFIG_PACKAGE_https-dns-proxy=y
+# " >> .config
 
 # ---------- sync config ----------
 make oldconfig
