4b236b0137b4657f9bbe3d326d46fb9fb4d7619b
Grinch27 <Grinch27@outlook.com>
2024-06-30 18:14:15 +0800

    debug compile

--- MediaTek_Ralink_MIPS/MT7620/diy-part2.sh
+++ MediaTek_Ralink_MIPS/MT7620/diy-part2.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# # https-dns-proxy
-# echo "
-# CONFIG_PACKAGE_luci-app-https-dns-proxy=y
-# CONFIG_PACKAGE_https-dns-proxy=y
-# " >> .config
-
 # ---------- sync config ----------
 make oldconfig
 cat ./.config
